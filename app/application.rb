9240e9ea0eca31b37ba70617d24c004bccce8df2
Zach  Smith <zdsmith001@gmail.com>
2019-10-29 16:42:39 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,19 +1,19 @@
 class Application
- 
+
   def call(env)
     resp = Rack::Response.new
- 
+
     num_1 = Kernel.rand(1..20)
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
- 
+
     if num_1==num_2 && num_2==num_3
       resp.write "You Win"
     else
       resp.write "You Lose"
     end
- 
+
     resp.finish
   end
- 
+
 end
